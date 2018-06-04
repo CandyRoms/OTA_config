90ee87b957d7e069f0ef7dd26181b3b7ec6bd5f9
bhi768 <bhimanand7@gmail.com>
2018-06-04 10:39:39 +0000

    Add mido folder
    Change-Id: Icf022f209db958d4fa3d26acd52af4500031f83f

---
+++ ota-gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+device=$1
+cd ~/bhi/OTA_config/bhi
+oldzip=$(grep "<Filename>Candy" $device.xml  | sed -e 's|</\?Filename>||g')
+newzip=$2
+sed -i "s/$oldzip/$newzip/g" $device.xml
+cd -
+cd ~/bhi/OTA_config/bhi
+echo "OTA Update for Candy $device $(date +%Y%m%d) Build" > /tmp/candyota
+git commit -as -F /tmp/candyota
+git pull --no-edit origin c8.1;
+git push origin c8.1
+cd -
+rm -fv /tmp/candyota

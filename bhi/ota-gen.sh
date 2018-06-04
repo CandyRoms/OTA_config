baba5d75add7e22f887ca25ea5bdf8776c68ca4f
bhi768 <bhimanand7@gmail.com>
2018-06-04 11:34:33 +0000

    OTA Update for Candy  20180604 Build
    Change-Id: Ie260397a0c9989461486e1ccb602797523a86d57
    Signed-off-by: bhi768 <bhimanand7@gmail.com>

--- bhi/ota-gen.sh
+++ bhi/ota-gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 device=$1
 cd ~/bhi/OTA_config/bhi
-oldzip=$(grep "<Filename>Candy-mido" mido.xml  | sed -e 's|</\?Filename>||g')
+oldzip=$(grep "<Filename>Candy-mido" $mido.xml  | sed -e 's|</\?Filename>||g')
 newzip=$2
-sed -i "s/$oldzip/$newzip/g" mido.xml
+sed -i "s/$oldzip/$newzip/g" $mido.xml
 cd -
 cd ~/bhi/OTA_config/bhi
 echo "OTA Update for Candy $device $(date +%Y%m%d) Build" > /tmp/candyota

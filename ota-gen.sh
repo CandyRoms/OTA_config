f8c0d9ece5a797f215811295054680bed424126c
bhi768 <bhimanand7@gmail.com>
2018-06-04 11:23:17 +0000

    OTA Update for Candy  20180604 Build
    Change-Id: I0331782e8ce632603b056675b8a6f24fb1ac3deb
    Signed-off-by: bhi768 <bhimanand7@gmail.com>

--- ota-gen.sh
+++ ota-gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 device=$1
-cd ~/OTA_config/bhi
+cd ~/bhi/OTA_config/bhi
 oldzip=$(grep "<Filename>Candy" $device.xml  | sed -e 's|</\?Filename>||g')
 newzip=$2
 sed -i "s/$oldzip/$newzip/g" $device.xml
 cd -
-cd ~/OTA_config/bhi
+cd ~/bhi/OTA_config/bhi
 echo "OTA Update for Candy $device $(date +%Y%m%d) Build" > /tmp/candyota
 git commit -as -F /tmp/candyota
 git pull --no-edit origin c8.1;
